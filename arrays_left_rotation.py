e6d4add4fe6f57dd6aabe7539e0b7c918f1f8b0b
Erica Bertan <ericabertan@gmail.com>
2022-09-27 18:47:01 -0300

    Solves Arrays: Left Rotation

---
+++ arrays_left_rotation.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER d
+#
+
+def rotLeft(a, d):
+    # Write your code here
+    tam = len(a)
+    rotVec = [0] * tam
+    
+    for i in range(0, tam):
+        j = i - d
+        if j < 0:
+            j = tam + j
+        
+        rotVec[j] = a[i]
+    
+    return rotVec
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
