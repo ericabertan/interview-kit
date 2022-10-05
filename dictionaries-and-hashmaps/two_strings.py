631d8962b7445dcc2034d06069130c84eae37a1d
Erica Bertan <ericabertan@gmail.com>
2022-10-05 06:07:03 -0300

    Create two_strings.py

---
+++ dictionaries-and-hashmaps/two_strings.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'twoStrings' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+def twoStrings(s1, s2):
+    dict_ = {} 
+    for ch in s1:
+        if dict_.get(ch) is None:
+            dict_[ch] = 1
+        else:
+            dict_[ch] += 1
+            
+    for ch in s2:
+        if dict_.get(ch) is not None:
+            return 'YES'
+    
+    return 'NO'
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
